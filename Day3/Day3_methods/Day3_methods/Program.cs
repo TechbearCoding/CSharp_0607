fd5ec48f4abeb843d1b07fd08241f7c0b3cf942d
male <martins@techspace.lv>
2021-07-20 20:15:58 +0300

    day 4

--- Day3/Day3_methods/Day3_methods/Program.cs
+++ Day3/Day3_methods/Day3_methods/Program.cs
@@ -6,11 +6,42 @@ namespace Day3_methods
     {
         static void Main(string[] args)
         {
-            Sum(1,2);
-            Sum(8, 99);
+            //Sum(1,2);
+            //Sum(8, 99);
 
-            int a = 6;
-            Sum(1, a);
+            //int a = 6;
+
+            //Sum(1, a);
+
+            //Console.WriteLine("Ievadiet 1. skaitli!");
+            //String input = Console.ReadLine();
+
+            //int skaitlis = Convert.ToInt32(input);
+
+            //Console.WriteLine("Ievadiet 1. skaitli!");
+            //input = Console.ReadLine();
+
+            //int skaitlis2 = Convert.ToInt32(input);
+
+            //Sum(skaitlis, skaitlis2);
+
+            Compare(1.4, 7.9);
+            Compare(1, 8.9);
+        }
+
+        static void Compare(double a, double b)
+        {
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            } else if (b > a)
+            {
+                Console.WriteLine(b);
+            }
+            else
+            {
+                Console.WriteLine("Skaitļi ir vienādi");
+            }
         }
 
         static void Sum(int a, int b)
---
+++ Day4/Day4_methods/Day4_methods/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Day4_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = NameInput();
+            Console.WriteLine(a);
+
+            String b = NameInput();
+            Console.WriteLine(b);
+        }
+
+
+        static String NameInput()
+        {
+            Console.WriteLine("Ievadiet cilvēka vārdu!");
+            String name = Console.ReadLine();
+
+            if(name.Length > 5)
+            {
+                return name;
+            }
+            else
+            {
+                return "empty";
+            }
+
+        }
+
+        static int SkaitlaIevade()
+        {
+            Console.WriteLine("Ievadiet skaitli!");
+            int skaitlis = Convert.ToInt32(Console.ReadLine());
+            return skaitlis;
+        }
+
+
+        static int Sum(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+
+
+
+
+
+        static void Complete(string a, string b, string c)
+        {
+            Console.WriteLine(a + b + c);
+
+            int totalsize = a.Length + b.Length + c.Length;
+
+            Console.WriteLine(totalsize);
+
+        }
+
+        static void Darbibas(int a)
+        {
+            int skaitlis = SkaitlaIevade();
+
+            int skaitlis2 = SkaitlaIevade();
+
+            if (a == 1)
+            {
+                Console.WriteLine(skaitlis2 + skaitlis);
+            }else if (a == 2)
+            {
+                Console.WriteLine(skaitlis - skaitlis2);
+            }else if (a == 3)
+            {
+                Console.WriteLine(skaitlis * skaitlis2);
+            }
+            else
+            {
+                Console.WriteLine("Skaitlim vajadzeja but no 1 lidz 3");
+            }
+        }
+    }
+}
