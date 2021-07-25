3a86d31d695139168802e6acab6080f6e5a0edbb
male <martins@techspace.lv>
2021-07-25 12:37:17 +0300

    day 6 samples

---
+++ Day6/Day6_MoreLoops/Day6_MoreLoops/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Day6_MoreLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uzdevums3();
+        }
+
+        static void Uzdevums3()
+        {
+            int biggest = 0;
+            for(int i = 0; i < 5; i++)
+            {
+                int current = NumberInput();
+
+                if (current > biggest)
+                {
+                    biggest = current;
+                }
+            }
+
+            Console.WriteLine("Lielakais skaitlis ir " + biggest);
+        }
+
+
+        static void Uzdevums2()
+        {
+            Console.WriteLine("Ievadiet skaitli!");
+            int fakt = Convert.ToInt32(Console.ReadLine());
+
+            int result = 1;
+            Console.Write(result);
+            for (int i = 2; i <= fakt; i++)
+            {
+                Console.Write("*" + i);
+                result = result * i;
+            }
+
+            Console.WriteLine(" = " + result);
+        }
+
+        static void Uzdevums1()
+        {
+            int wins = 0;
+
+            for(int i = 0; i < 5; i++) { 
+                Random random = new Random();
+
+                int randomNumber = random.Next(1, 11);
+
+                int guess = NumberInput();
+
+                if (randomNumber == guess)
+                {
+                    Console.WriteLine("Spele ir uzvareta!");
+                    wins++; //wins = wins + 1
+                }
+                else
+                {
+                    Console.WriteLine("Spele zaudeta, istais skaitlis ir " + randomNumber);
+                }
+            }
+
+            Console.WriteLine("Uzvaras- " + wins);
+            Console.WriteLine("Zaudejumi- " + (5 - wins));
+
+        }
+
+        static int NumberInput()
+        {
+            Console.WriteLine("Ievadiet skaitli!");
+
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
