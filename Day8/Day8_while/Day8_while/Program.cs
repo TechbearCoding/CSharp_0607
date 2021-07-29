ef6990a4688071541e6d4f043e37ff63777c19d7
male <martins@techspace.lv>
2021-07-29 19:06:01 +0300

    day 8

---
+++ Day8/Day8_while/Day8_while/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day8_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
