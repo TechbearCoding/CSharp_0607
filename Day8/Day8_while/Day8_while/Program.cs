5f9b08657325d1bd11175eaecc525cdefe6bbd95
male <martins@techspace.lv>
2021-07-29 19:23:10 +0300

    samples

--- Day8/Day8_while/Day8_while/Program.cs
+++ Day8/Day8_while/Day8_while/Program.cs
@@ -6,7 +6,28 @@ namespace Day8_while
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Uzdevums1();
+        }
+
+        static void Uzdevums1()
+        {
+            int sum = 0;
+
+            String choice = "";
+
+            while(choice != "exit")
+            {
+                Console.WriteLine("Ievadiet skaitli!");
+                int sk = Convert.ToInt32(Console.ReadLine());
+
+                sum = sum + sk;
+
+                Console.WriteLine("Vai velaties turpinat darbu?");
+                choice = Console.ReadLine();
+
+            }
+            
+            Console.WriteLine(sum);
         }
     }
 }
