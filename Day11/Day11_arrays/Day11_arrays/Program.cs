0eaddd10a103a7f46a608c31f68b8cb33c7d93fa
male <martins@techspace.lv>
2021-08-31 19:36:56 +0300

    day 11 samples

--- Day11/Day11_arrays/Day11_arrays/Program.cs
+++ Day11/Day11_arrays/Day11_arrays/Program.cs
@@ -6,7 +6,31 @@ namespace Day11_arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Piemers1();
+        }
+
+        static void Piemers1()
+        {
+            int[] testArray = new int[5];
+
+            testArray[0] = 2;
+            testArray[1] = 4;
+
+            testArray[2] = 32;
+            testArray[3] = 6;
+
+            testArray[4] = 91;
+
+            for(int i = 0; i < testArray.Length; i++)
+                Console.WriteLine(testArray[i]);
+
+            String[] testString = { "aa", "bb", "cc", "aa1" };
+
+            Console.WriteLine();
+
+            for (int i = 0; i < testString.Length; i++)
+                Console.WriteLine(testString[i]);
+
         }
     }
 }
