8af3419a558499fe34182eab026241842156e7c1
Martins <martins@techspace.lv>
2021-09-16 19:58:49 +0300

    day 15 task 1

---
+++ Day15/Day15/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            List<int> saraksts = new List<int>();
+
+            int randomnumber = 0;
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 30; i++)
+            {
+                randomnumber = random.Next(0, 11);
+
+                saraksts.Add(randomnumber);
+
+                Console.Write(saraksts[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 30; i++)
+            {
+                //Console.WriteLine(saraksts[i]);
+
+                if (saraksts[i] == input)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
