65ca6ae2c62b3b4085f2ddef0fdd64e39be8ce20
Martins <martins@techspace.lv>
2021-10-05 19:41:27 +0300

    homework

---
+++ Day19_homework/Day19_homework/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Day19_homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.btnAdd1 = new System.Windows.Forms.Button();
+            this.btnDel1 = new System.Windows.Forms.Button();
+            this.btnDel2 = new System.Windows.Forms.Button();
+            this.btnAdd2 = new System.Windows.Forms.Button();
+            this.btnDel3 = new System.Windows.Forms.Button();
+            this.btnAdd3 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(49, 82);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(328, 260);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(431, 82);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(328, 260);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(804, 82);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(328, 260);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // btnAdd1
+            // 
+            this.btnAdd1.Location = new System.Drawing.Point(49, 375);
+            this.btnAdd1.Name = "btnAdd1";
+            this.btnAdd1.Size = new System.Drawing.Size(328, 51);
+            this.btnAdd1.TabIndex = 3;
+            this.btnAdd1.Text = "Pievienot";
+            this.btnAdd1.UseVisualStyleBackColor = true;
+            this.btnAdd1.Click += new System.EventHandler(this.btnAdd1_Click);
+            // 
+            // btnDel1
+            // 
+            this.btnDel1.Location = new System.Drawing.Point(49, 453);
+            this.btnDel1.Name = "btnDel1";
+            this.btnDel1.Size = new System.Drawing.Size(328, 51);
+            this.btnDel1.TabIndex = 4;
+            this.btnDel1.Text = "Dzēst";
+            this.btnDel1.UseVisualStyleBackColor = true;
+            this.btnDel1.Click += new System.EventHandler(this.btnDel1_Click);
+            // 
+            // btnDel2
+            // 
+            this.btnDel2.Location = new System.Drawing.Point(431, 453);
+            this.btnDel2.Name = "btnDel2";
+            this.btnDel2.Size = new System.Drawing.Size(328, 51);
+            this.btnDel2.TabIndex = 6;
+            this.btnDel2.Text = "Dzēst";
+            this.btnDel2.UseVisualStyleBackColor = true;
+            this.btnDel2.Click += new System.EventHandler(this.btnDel2_Click);
+            // 
+            // btnAdd2
+            // 
+            this.btnAdd2.Location = new System.Drawing.Point(431, 375);
+            this.btnAdd2.Name = "btnAdd2";
+            this.btnAdd2.Size = new System.Drawing.Size(328, 51);
+            this.btnAdd2.TabIndex = 5;
+            this.btnAdd2.Text = "Pievienot";
+            this.btnAdd2.UseVisualStyleBackColor = true;
+            this.btnAdd2.Click += new System.EventHandler(this.btnAdd2_Click);
+            // 
+            // btnDel3
+            // 
+            this.btnDel3.Location = new System.Drawing.Point(804, 453);
+            this.btnDel3.Name = "btnDel3";
+            this.btnDel3.Size = new System.Drawing.Size(328, 51);
+            this.btnDel3.TabIndex = 8;
+            this.btnDel3.Text = "Dzēst";
+            this.btnDel3.UseVisualStyleBackColor = true;
+            this.btnDel3.Click += new System.EventHandler(this.btnDel3_Click);
+            // 
+            // btnAdd3
+            // 
+            this.btnAdd3.Location = new System.Drawing.Point(804, 375);
+            this.btnAdd3.Name = "btnAdd3";
+            this.btnAdd3.Size = new System.Drawing.Size(328, 51);
+            this.btnAdd3.TabIndex = 7;
+            this.btnAdd3.Text = "Pievienot";
+            this.btnAdd3.UseVisualStyleBackColor = true;
+            this.btnAdd3.Click += new System.EventHandler(this.btnAdd3_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(49, 542);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(444, 238);
+            this.listView1.TabIndex = 9;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1189, 788);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btnDel3);
+            this.Controls.Add(this.btnAdd3);
+            this.Controls.Add(this.btnDel2);
+            this.Controls.Add(this.btnAdd2);
+            this.Controls.Add(this.btnDel1);
+            this.Controls.Add(this.btnAdd1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button btnAdd1;
+        private System.Windows.Forms.Button btnDel1;
+        private System.Windows.Forms.Button btnDel2;
+        private System.Windows.Forms.Button btnAdd2;
+        private System.Windows.Forms.Button btnDel3;
+        private System.Windows.Forms.Button btnAdd3;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Day19_homework/Day19_homework/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Day19_homework
+{
+    public partial class Form1 : Form
+    {
+        List<String> Notes = new List<String>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void btnAdd1_Click(object sender, EventArgs e)
+        {
+            AddElement(richTextBox1.Text);
+        }
+
+        private void btnAdd2_Click(object sender, EventArgs e)
+        {
+            AddElement(richTextBox2.Text);
+        }
+
+        private void btnAdd3_Click(object sender, EventArgs e)
+        {
+            AddElement(richTextBox3.Text);
+        }
+
+        private void btnDel1_Click(object sender, EventArgs e)
+        {
+            DeleteElement(richTextBox1.Text);
+        }
+
+        private void btnDel2_Click(object sender, EventArgs e)
+        {
+            DeleteElement(richTextBox2.Text);
+        }
+
+        private void btnDel3_Click(object sender, EventArgs e)
+        {
+            DeleteElement(richTextBox3.Text);
+        }
+
+        private void AddElement(String txt)
+        {
+            if (txt != "" && Notes.Count != 3 && !IsPresent(txt))
+            {
+                Notes.Add(txt);
+            }
+            DisplayList();
+        }
+
+
+        private void DeleteElement(String txt)
+        {
+            Notes.RemoveAt(Notes.IndexOf(txt));
+            DisplayList();
+        }
+
+        private void DisplayList()
+        {
+            listView1.Items.Clear();
+            for (int i = 0; i < Notes.Count; i++)
+            {
+                listView1.Items.Add(Notes[i]);
+            }
+        }
+
+        private bool IsPresent(String txt)
+        {
+            for(int i = 0; i < Notes.Count; i++)
+            {
+                if(Notes[i] == txt)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
