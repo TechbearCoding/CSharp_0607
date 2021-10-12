58b6340dbfcd5f6d007fab456565567a7dc333bf
Martins <martins@techspace.lv>
2021-10-12 19:31:35 +0300

    day 22

---
+++ Day22_OOP/Day22_OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Day22_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Jānis", "Bērziņš", 1993, 9.6);
+            s1.Print();
+
+            Student s2 = new Student("Juris", "Ābols", 1991, 7.1);
+            s2.Print();
+        }
+    }
+}
---
+++ Day22_OOP/Day22_OOP/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22_OOP
+{
+    class Student
+    {
+        public String name;
+        public String lastName;
+        public int year;
+        public double avarage;
+
+        public Student(String name, String lastName, int year, double avarage)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            this.year = year;
+            this.avarage = avarage;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(name + " " + lastName);
+            Console.WriteLine("Year- " + year);
+            Console.WriteLine("Avarage- " + avarage);
+        }
+    }
+}
