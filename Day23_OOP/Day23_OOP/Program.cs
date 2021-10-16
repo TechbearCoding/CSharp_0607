53fc746abfeb6c9365bff5146ba0da7550e720a9
Martins <martins@techspace.lv>
2021-10-16 13:40:53 +0300

    get/set samples

---
+++ Day23_OOP/Day23_OOP/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day23_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Jānis", "Bērziņš", 1993, 9.6);
+            s1.Print();
+
+            s1.name = "Juris";
+            s1.SetYear(1950);
+            s1.lastName = "Ābols";
+        }
+    }
+}
---
+++ Day23_OOP/Day23_OOP/Student.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23_OOP
+{
+    class Student
+    {
+        public String name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                if (name.Length > 10) 
+                {
+                    name = "default";
+                }
+                else
+                {
+                     name = value;
+                }
+            }
+        }
+        public String lastName { get; set; }
+        private int year;
+        public double avarage { get; set; }
+
+        public Student(String name, String lastName, int year, double avarage)
+        {
+            this.name = name;
+            this.lastName = lastName;
+            SetYear(year);
+            this.avarage = avarage;
+        }
+
+        public int GetYear()
+        {
+            return year;
+        }
+
+        public void SetYear(int year)
+        {
+            if(year > DateTime.Now.Year)
+            {
+                this.year = DateTime.Now.Year;
+            }else if(year < 1920)
+            {
+                this.year = 1920;
+            }
+            else
+            {
+                this.year = year;
+            }
+            
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(name + " " + lastName);
+            Console.WriteLine("Year- " + year);
+            Console.WriteLine("Avarage- " + avarage);
+        }
+    }
+}
