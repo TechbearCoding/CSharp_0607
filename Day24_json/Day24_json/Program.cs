8c49cb796f1fef842f5738cd62e2f4111ac6c434
Techbear <21012644+TechbearCoding@users.noreply.github.com>
2021-10-21 20:48:39 +0300

    day 24

---
+++ Day24_json/Day24_json/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Day24_json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static void Sample1()
+        {
+            Student st = new Student();
+            st.InitStudent("Juris", "Abele", 2);
+
+            String jsonString = JsonSerializer.Serialize(st);
+
+            Console.WriteLine(jsonString);
+
+            Student st2 = JsonSerializer.Deserialize<Student>(jsonString);
+            st2.Print();
+        }
+    }
+}
