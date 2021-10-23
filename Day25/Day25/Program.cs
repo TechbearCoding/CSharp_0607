269ae009ccb27c8aa77618b46dabd4c170e435ba
Martins <martins@techspace.lv>
2021-10-23 11:40:10 +0300

    more samples

--- Day25/Day25/Program.cs
+++ Day25/Day25/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace Day25
 {
@@ -7,7 +9,20 @@ namespace Day25
         static void Main(string[] args)
         {
             //Homework.Rakstitajs();
-            Homework.Lasitajs();
+            List<User> lstUsr = new List<User>();
+            lstUsr.Add(new User());
+            lstUsr[0].InitUser("user", "user", "user", "user", 2011);
+
+            lstUsr.Add(new User());
+            lstUsr[1].InitUser("user2", "user2", "user2", "user2", 2012);
+
+            String jsonString = JsonSerializer.Serialize(lstUsr);
+            Console.WriteLine(jsonString);
+
+
+            List<User> lstUsr2 = JsonSerializer.Deserialize<List<User>>(jsonString);
+
+            Console.WriteLine(lstUsr2[0].name);
         }
     }
 }
