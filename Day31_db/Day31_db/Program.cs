025e1c4b890c52c58c77d419b8362b59582de75f
Martins <martins@techspace.lv>
2021-11-25 20:22:17 +0200

    database test

---
+++ Day31_db/Day31_db/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace Day31_db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Connect();
+        }
+
+        static void Connect()
+        {
+            var settings = new MySqlConnectionStringBuilder
+            {
+                Server = "178.128.47.243",
+                UserID = "root",
+                Password = "danger123",
+                Port = 3306,
+                Database = "Books"
+            };
+
+
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(settings.ConnectionString);
+                conn.Open();
+
+                Console.WriteLine("Connection successful");
+            }
+            catch
+            {
+                Console.WriteLine("Connection failed!");
+
+            }
+
+
+        }
+    }
+}
